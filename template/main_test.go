4e434192ebb0268d20eb45f1d8b78074363ee28d
Abel Tay <15604207+abeltay@users.noreply.github.com>
2023-12-04 16:21:34 +0800

    Use t.Fatalf so that it ends when errored

--- template/main_test.go
+++ template/main_test.go
@@ -10,12 +10,12 @@ func TestParts(t *testing.T) {
 	got := part1(testFile)
 	want := 0
 	if got != want {
-		t.Errorf("got %v want %v", got, want)
+		t.Fatalf("got %v want %v", got, want)
 	}
 	// got2 := part2(testFile)
 	// want2 := 0
 	// if got2 != want2 {
-	// 	t.Errorf("got %v want %v", got2, want2)
+	// 	t.Fatalf("got %v want %v", got2, want2)
 	// }
 
 	fmt.Println("Running input.txt")
