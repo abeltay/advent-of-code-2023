b42ca3c0472e804590c23c8e5bf285c47fc7307c
Abel Tay <15604207+abeltay@users.noreply.github.com>
2023-12-22 17:45:11 +0800

    Add ParseFile to utilities

--- template/main.go
+++ template/main.go
@@ -1,27 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+
+	"github.com/abeltay/advent-of-code-2023/utilities"
 )
-
-func parseFile(filename string) []string {
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer file.Close()
-
-	var arr []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		t := scanner.Text()
-		arr = append(arr, t)
-	}
-	return arr
-}
 
 /*
 type line struct {
@@ -32,7 +15,7 @@ type line struct {
 */
 
 func parseInput(filename string) [][]byte {
-	in := parseFile(filename)
+	in := utilities.ParseFile(filename)
 
 	var arr [][]byte
 	for _, row := range in {
