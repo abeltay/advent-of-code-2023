73f5f6b800c4219961fe896dccc2711066fbe2e0
Abel Tay <15604207+abeltay@users.noreply.github.com>
2023-12-27 22:57:20 +0800

    Day 25: Snowverload

--- 25/main_test.go
+++ 25/main_test.go
@@ -14,6 +14,6 @@ func TestParts(t *testing.T) {
 	}
 
 	fmt.Println("Running input.txt")
-	actualFile := "testdata/input.txt"
-	fmt.Println("Answer for part 1:", part1(actualFile))
+	// actualFile := "testdata/input.txt"
+	// fmt.Println("Answer for part 1:", part1(actualFile))
 }
